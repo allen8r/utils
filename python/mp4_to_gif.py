8f64137f5439b36401085a19988afe4cc0b9931f
Allen C. Lau <allen.c.lau@gmail.com>
2019-04-08 17:21:39 -0700

    Add .mp4 restriction as input file. Add command usage details.

--- python/mp4_to_gif.py
+++ python/mp4_to_gif.py
@@ -1,9 +1,13 @@
 #!/home/alau/anaconda3/envs/sdc-lanes/bin/python
+#
+# Usage: ./video-capture.py <filename> <start(sec)> <end(sec)>
+#
+
 from sys import argv
 from moviepy.editor import VideoFileClip
 
 ##### Get values from command line #####
-name = argv[1]
+filename = argv[1]
 begin = int(argv[2])
 end = int(argv[3])
 ########################################
@@ -27,9 +31,14 @@ end = int(argv[3])
 #
 #########################################
 
-vclip = VideoFileClip(f"./{name}.mp4").subclip(begin, end)
+filename_parts = filename.split('.')
+ext = filename_parts[-1]
+name = "".join(filename_parts[:-1])
+
+if ext != 'mp4':
+  print(f'\n>>EXIT: {filename} is not a .mp4 file.\nOnly .mp4 video files can be used as input.')
+  raise SystemExit
+
+vclip = VideoFileClip(f"./{filename}").subclip(begin, end)
 #vclip.write_gif(f"./{name}.gif", fps=3, program='imageio' loop=1, fuzz=0)
-vclip.write_gif(f"./{name}.gif", fps=4, program='ffmpeg', loop=1, fuzz=0)
-
-
-
+vclip.write_gif(f"./{name}.gif", fps=4, program='ffmpeg', loop=1, fuzz=1)
